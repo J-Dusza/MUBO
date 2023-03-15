61c391c1e453636d75ef487f8c3bf463eb004922
Jan Dusza <duszajan@icloud.com>
2023-03-15 01:57:26 +0100

    Working form validation, login validation, connected to firebase

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAvH48eE2vl5N0JMUZtRony4RF75Zl2Mg",
+  authDomain: "muno-clothing.firebaseapp.com",
+  projectId: "muno-clothing",
+  storageBucket: "muno-clothing.appspot.com",
+  messagingSenderId: "953488615110",
+  appId: "1:953488615110:web:cac581bc8e53f51d0b19c7",
+};
+
+const app = initializeApp(firebaseConfig);
