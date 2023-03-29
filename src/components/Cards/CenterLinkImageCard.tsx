da790021fda6bedf0d5987db9c8f3b28a2073720
Jan Dusza <duszajan@icloud.com>
2023-03-29 13:35:13 +0200

    center link page Image Next js optim

--- src/components/Cards/CenterLinkImageCard.tsx
+++ src/components/Cards/CenterLinkImageCard.tsx
@@ -15,11 +15,17 @@ const CenterLinkImageCard = ({ name, href, imgUrl, className }: Props) => {
       className={`${className} relative flex items-center justify-center shadow-2xl`}
       href={href}
     >
-      <img
-        src={imgUrl}
-        alt={name}
-        className=" object-cover aspect-video sm:aspect-square"
-      />
+      <div className=" object-cover aspect-video sm:aspect-square h-full w-full">
+        <Image
+          objectFit="cover"
+          sizes="(min-width: 60em) 24vw,
+                    (min-width: 28em) 45vw,
+                    100vw"
+          fill
+          src={imgUrl}
+          alt={name}
+        />
+      </div>
       <button className="absolute btn btn-xl btn-primary px-8 border-white border-2 text-white">
         {name}
       </button>
